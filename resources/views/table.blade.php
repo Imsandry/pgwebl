c5b999fd35fe46c8e8a7c3d3555ec113d99da12e
Imsandry <andrynur912@gmail.com>
2025-03-12 02:18:16 +0700

    first commit

---
+++ resources/views/table.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.template')
+
+@section('content')
+<table class="table table-striped">
+    <thead>
+        <tr>    
+            <th>No</th>
+            <th>Name</th>
+            <th>Nim</th>
+            <th>Kelas</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>1</th>
+            <th>Imsandry</th>
+            <th>23/518883/SV/23082</th>
+            <th>SIG B</th>
+        </tr>
+        <tr>
+            <th>2</th>
+            <th>Naufal</th>
+            <th>520500</th>
+            <th>SIG B</th>
+        </tr>
+        <tr>
+            <th>1</th>
+            <th>Irfan</th>
+            <th>518439</th>
+            <th>SIG B</th>
+        </tr>
+    </tbody>
+</table>
+@endsection
